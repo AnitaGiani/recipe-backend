8d28381cbadbfbab7ad048aa494fdecf10ccbdd3
AnitaGiani <anita.giani2014@gmail.com>
2025-07-08 14:50:37 +0800

    fix: added correct MongoDB URI with database name

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 // Use environment variable for MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('MongoDB Connection Error:', err));
 
 const RecipeSchema = new mongoose.Schema({
   title: String,
